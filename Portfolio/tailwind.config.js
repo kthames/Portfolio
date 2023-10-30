b56f8c7c74d0728fbfc1f7ae5fe375229ea23fb7
kthames <135296109+kthames@users.noreply.github.com>
2023-10-30 13:51:09 -0500

    updated tailwind config

--- Portfolio/tailwind.config.js
+++ Portfolio/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+  content: ["./index.html","./src/**/*.{html,js,jsx}"],
   mode: "jit",
   theme: {
     extend: {
